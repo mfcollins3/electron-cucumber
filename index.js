6488bec9ca395ac4c20dd5d9ef5f4db84699fbcd
ido roseman <ido.roseman@gmail.com>
2019-02-12 23:51:27 +0200

    fixed bug that caused app to close prematurly on osx

--- index.js
+++ index.js
@@ -81,6 +81,14 @@ app.on('ready', function() {
 
 });
 
+app.on('window-all-closed', function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
 function exit(code) {
   fs.remove(browserDataPath, function(err) {
     if (err) {
